03a778ed74b4b0f672a8a0f46eae7e7d89e13198
Shaun Chong <data-miner00@users.noreply.github.com>
2025-02-15 09:23:47 +0800

    feat: Working pie chart

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,23 @@
 import { UpperCasePipe, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
+import { ChartType, GoogleChartsModule } from 'angular-google-charts';
+
 @Component({
   selector: 'app-home',
-  imports: [UpperCasePipe, DatePipe],
+  imports: [UpperCasePipe, DatePipe, GoogleChartsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   now = new Date();
+  data = [
+    ['Work', 11],
+    ['Eat', 2],
+    ['Commute', 2],
+    ['Watch TV', 2],
+    ['Sleep', 7],
+  ];
+  columns = ['Task', 'Hours per Day'];
+  type = ChartType.PieChart;
 }
