aadb6ae8060f904be69e2e63410442f5a4638304
Shaun Chong <data-miner00@users.noreply.github.com>
2025-03-06 22:38:13 +0800

    feat: Add simple sidebar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,17 @@ import { RouterOutlet } from '@angular/router';
 
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, HeaderComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
