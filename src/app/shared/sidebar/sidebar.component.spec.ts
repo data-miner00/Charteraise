08388e9f94050b3523157071173afbeba3cb4a1b
Shaun Chong <data-miner00@users.noreply.github.com>
2025-03-27 17:11:57 +0800

    feat: Dynamic sidebar

--- src/app/shared/sidebar/sidebar.component.spec.ts
+++ src/app/shared/sidebar/sidebar.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 
 import { SidebarComponent } from './sidebar.component';
 
@@ -8,9 +9,20 @@ describe('SidebarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SidebarComponent]
-    })
-    .compileComponents();
+      imports: [SidebarComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {
+                title: 'Test Title',
+              },
+            },
+          },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+type Link = {
+  url: string;
+  name: string;
+  icon: string;
+};
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  links: Link[] = [
+    {
+      url: '/line-charts',
+      name: 'Line Charts',
+      icon: 'bi-graph-up',
+    },
+    {
+      url: '/bar-charts',
+      name: 'Bar Charts',
+      icon: 'bi-bar-chart',
+    },
+    {
+      url: '/gauge-charts',
+      name: 'Gauge Charts',
+      icon: 'bi-speedometer2',
+    },
+    {
+      url: '/geo-charts',
+      name: 'Geo Charts',
+      icon: 'bi-globe-americas',
+    },
+    {
+      url: '/pie-charts',
+      name: 'Pie Charts',
+      icon: 'bi-pie-chart',
+    },
+  ];
+}
