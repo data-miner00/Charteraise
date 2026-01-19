6d02a843bd8293c6d8f052eefed53539297d77fb
Shaun Chong <data-miner00@users.noreply.github.com>
2026-01-19 22:31:54 +0800

    chore: Remove unused footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
-import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    FormsModule,
-    HeaderComponent,
-    FooterComponent,
-    SidebarComponent,
-  ],
+  imports: [RouterOutlet, FormsModule, HeaderComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
